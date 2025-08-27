81847d9e5fd5798f47399d58128832ca6bd8ba9b
kotaro-kadoshima <kadoshima.kotaro+dev@gmail.com>
2025-08-27 23:28:03 +0900

    first init

---
+++ src/main/java/com/cinemo/api/CinemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.cinemo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinemo/api/CinemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
