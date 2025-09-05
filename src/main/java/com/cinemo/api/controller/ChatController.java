332f0f7187f8dfe2debfcb7e334480c3570b7abc
kotaro-kadoshima <kadoshima.kotaro+dev@gmail.com>
2025-09-05 19:44:57 +0900

    feat: Geminiの設定

--- src/main/java/com/cinemo/api/controller/ChatController.java
+++ src/main/java/com/cinemo/api/controller/ChatController.java
@@ -1,21 +1,27 @@
 package com.cinemo.api.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class ChatController {
 
     private final OllamaChatModel ollama;
+    private final VertexAiGeminiChatModel gemini;
 
-    public ChatController(OllamaChatModel ollama) {
-        this.ollama = ollama;
-    }
 
     @GetMapping("/ollama")
     public String chatOllama(@RequestParam String message) {
         return ollama.call(message);
+    }
+
+    @GetMapping("/gemini")
+    public String chatGemini(@RequestParam String message) {
+        return gemini.call(message);
     }
 }
